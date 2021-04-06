c89fe644e43851d8d66753de473a5d3e12f2c8ba
Dan Seremet <dan_seremet@yahoo.com>
2021-04-06 13:51:51 -0400

    feat(#254): Comment Notification to Reviewers

---
+++ app/Observers/CommentObserver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Comment;
+use App\Notifications\CommentNotification;
+use Illuminate\Support\Facades\Auth;
+
+class CommentObserver
+{
+    public function saved(Comment $comment)
+    {
+        if(Auth::check())
+        {
+            $booking = $comment->booking()->first();
+            $reviewers = $booking->reviewers()->get();
+            foreach ($reviewers as $reviewer) {
+                $reviewer->notify(new CommentNotification($comment));
+            }
+        }
+    }
+}
